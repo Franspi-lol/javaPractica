c57f6eb16a6b7a2d0b99730e7df6220a8a7bb222
Franco Bernardele <francobernar@gmail.com>
2024-02-09 17:41:56 +0000

    faf# Please enter the commit message for your changes. Lines starting

--- com/franco/arrays/MainArrays.java
+++ com/franco/arrays/MainArrays.java
@@ -1,10 +1,12 @@
 package com.franco.arrays;
 
+import java.util.Arrays;
+
 public class MainArrays {
     public static void main(String[] args) {
         int[] arreglo = new int[10];
         //System.out.println(arreglo.length);//length devulve la dimension del arreglo
-        cargarArreglo(arreglo);
+        /*cargarArreglo(arreglo);
         mostrarArregloForEach(arreglo);
         mostrarArregloValidos(arreglo);
 
@@ -12,7 +14,43 @@ public class MainArrays {
         for (String nombre : nombres) {
             System.out.println("nombre="+ nombre);
         }
-        //System.out.println(nombres[0]);
+
+        var nombre = "Franco";//var es una variable que se le asigna el tipo de dato automaticamente
+        if (nombre instanceof String) {//instanceof devuelve true si el objeto es de la clase que se compara
+            System.out.println("Es un string");
+            
+        }
+
+        String mensaje = nombre instanceof String ? "Es un string" : "No es un string";//operador ternario
+        System.out.println(mensaje);
+        //operador ternario funciona como un if else, mensaje = condicion ? valor si es verdadero : valor si es falso
+
+
+        //System.out.println(nombres[0]);*/
+
+        String[] juegos = {"Fornite", "PUBG", "FreeFire", "Call of Duty", "GTA V", "Minecraft", "Among Us", "FIFA 21", "PES 21", "NBA 2K21"};
+        mostrarArreglo(juegos);
+        
+        
+
+        //ordenar arreglo
+        Arrays.sort(juegos);//Arrays.sort(arreglo) para ordenar el arreglo, God bless java
+        
+        //iterar el arreglo
+        mostrarArreglo(juegos);
+
+        /*for (String string : juegos) {
+            
+            System.out.println(string);
+            
+        }*/
+        //buscar en el arreglo
+        for (int i = 0; i < juegos.length; i++) {
+            
+        }
+
+
+
     }
 
     public static void cargarArreglo(int[] arreglo) {
@@ -21,13 +59,14 @@ public class MainArrays {
         }
     }
 
-    public static void mostrarArreglo(int[] arreglo) {
+    public static void mostrarArreglo(String[] arreglo) {//metodo sobrecargado
         for (int i = 0; i < arreglo.length; i++) {//for normal fori
             System.out.println("arreglo="+ arreglo[i]);
         }
+        System.out.println("------");
     }
 
-    public static void mostrarArregloForEach(int[] arreglo) {
+    public static void mostrarArreglo(int[] arreglo) {//metodo sobrecargado
         for (int numero : arreglo) {//for each
             System.out.println("arreglo="+ numero);
         }
@@ -41,5 +80,7 @@ public class MainArrays {
             }
         }
     }
+
+
 
 }
